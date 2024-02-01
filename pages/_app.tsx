6e0d132103e5ef8c631820b7b2c5804f4a2de053
archamplamp <backendfujifilm@gmail.com>
2024-02-01 14:40:12 +0700

    chang title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-      <title>FUJIFILM</title>
-    </Head>
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <title>FFOTOBOX</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
